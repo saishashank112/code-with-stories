df5449a7bfe69fe17eb36c17a5204ea7897d90e0
Sai Shashank <129254218+saishashank112@users.noreply.github.com>
2025-07-04 06:06:53 +0000

    files created

---
+++ python/basics/1. Variables.py
@@ -0,0 +1,11 @@
+# 1. Variables
+# Definition: Variables are containers for storing data values.
+# Scenario: A food delivery app stores a customer’s name (str), the order amount (float), and delivery status (bool).
+
+name = "Shiva"
+total = 299.99
+delivered = False
+# Example: Printing the variables
+print("Customer Name:", name)
+print("Order Amount:", total)
+print("Delivered:", delivered)
